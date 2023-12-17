91b6c2086bdc3df69577ed5c45c4065ea337dfcf
William Thompson <wmthmps1@gmail.com>
2023-12-17 00:57:21 -0600

    Initial Commit

---
+++ Classes/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Classes
+{
+	public class Car
+	{
+		public Car()
+		{
+		}
+        public string Make { get; set; }
+        public string Model { get; set; }
+		public int Year { get; set; }
+
+    }
+}
+
+/*
+Create a Make property of type string that is public
+Create a Model property of type string that is public
+Create a Year property of type int that is public
+ */
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car firstCar = new Car();
+            firstCar.Make = "Honda";
+            firstCar.Model = "Accord";
+            firstCar.Year = 2001;
+            Console.WriteLine($"My first car was a {firstCar.Year} {firstCar.Make} {firstCar.Model}!");
         }
     }
 }
